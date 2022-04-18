75067a8caba4523619cf8bb274393ec4058d077b
Farzad <farzad.mobasher@hva.nl>
2022-04-18 18:15:32 +0200

    table added + userspage

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template
+from flask import Flask, render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    user = {'username': 'Dasraf'}
+    return '''
+<html>
+    <head>
+        <title>User Page</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+@app.route('/users')
+def users():
+    return render_template('users.html')
--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,5 @@
 from app import app
+from flask import Flask
+app = Flask(__name__)
+
+from app import routes
