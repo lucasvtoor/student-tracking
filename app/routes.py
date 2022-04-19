91b91c50187a2e041d05fa2138b215ef6ac87a4b
silvacm <miguel.da.silva.crespim@hva.nl>
2022-04-19 14:46:49 +0200

    added img and top header

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template
 from app import app
 
+# base application route
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
+
+# overview route
+@app.route('/overview')
+def overview():
+    return render_template('overview.html')
