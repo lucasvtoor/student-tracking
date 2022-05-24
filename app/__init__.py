1a0931506b04fb106c6d4b3eee0b10ef03eb66d3
Lucas van Toorenburg <lucas.van.toorenburg@hva.nl>
2022-05-24 17:03:20 +0200

    Update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,15 @@ from app.static.py.badgecraft import BadgeCraft
 
 app = Flask(__name__)
 
-scheduler = APScheduler()
-scheduler.add_job(id='Fetches Data from Badgecraft', func=commands.scheduled, trigger='interval', seconds=600)
-scheduler.start()
-badgecraft = BadgeCraft()
-
 
 def scheduled(token):
     badgecraft.fetched_amount = badgecraft.fetched_amount + 1
     print(badgecraft.fetched_amount)
 
     badgecraft.fetch(token)
+
+
+scheduler = APScheduler()
+scheduler.add_job(id='Fetches Data from Badgecraft', func=scheduled, trigger='interval', seconds=600)
+scheduler.start()
+badgecraft = BadgeCraft()
