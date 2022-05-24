6aac43b007fb6f9e7b3456d63b1def2ea09a116d
Lucas van Toorenburg <lucasvtoor@gmail.com>
2022-05-24 17:00:36 +0200

    fixed circular imports

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,15 @@ from flask_apscheduler import APScheduler
 from app.static.py.badgecraft import BadgeCraft
 
 app = Flask(__name__)
-from . import routes, commands
 
 scheduler = APScheduler()
 scheduler.add_job(id='Fetches Data from Badgecraft', func=commands.scheduled, trigger='interval', seconds=600)
 scheduler.start()
 badgecraft = BadgeCraft()
+
+
+def scheduled(token):
+    badgecraft.fetched_amount = badgecraft.fetched_amount + 1
+    print(badgecraft.fetched_amount)
+
+    badgecraft.fetch(token)
--- app/commands.py
+++ app/commands.py
@@ -1,13 +1,2 @@
-import click
-from app import app, badgecraft
-
-
-@app.cli.command("scheduled")
-@click.argument('token')
-def scheduled(token):
-    badgecraft.fetched_amount = badgecraft.fetched_amount + 1
-    print(badgecraft.fetched_amount)
-
-    badgecraft.fetch(token)
 
 
